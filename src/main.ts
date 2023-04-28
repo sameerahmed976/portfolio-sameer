9ea6c5ac0e1d6b50d10dff374516c56a9438a1ed
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2023-04-28 12:22:42 +0530

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import "./styles.scss";
+
+/* menu button   */
+
+const menuIcon = document.querySelector(".menu");
+const themeIcon = document.querySelector(".theme__icon");
+const navbarLinks = document.querySelector(".navbar__links");
+
+const prefersColorScheme = window.matchMedia("(prefers-color-scheme : dark)");
+
+menuIcon?.addEventListener("click", () => {
+  menuIcon.classList.toggle("active");
+  navbarLinks?.classList.toggle("navbar__links__active");
+});
+
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+  document.body.classList.toggle("dark__theme");
+} else if (currentTheme === "light") {
+  document.body.classList.toggle("light__theme");
+}
+
+themeIcon?.addEventListener("click", () => {
+  if (prefersColorScheme.matches) {
+    document.body.classList.toggle("light__theme");
+    themeIcon.classList.toggle("theme__icon__dark");
+    if (document.body.classList.contains("light__theme")) {
+      var theme = "light";
+    } else {
+      var theme = "dark";
+    }
+  } else {
+    document.body.classList.toggle("dark__theme");
+    themeIcon.classList.toggle("theme__icon__dark");
+
+    if (document.body.classList.contains("dark__theme")) {
+      var theme = "dark";
+    } else {
+      var theme = "light";
+    }
+  }
+
+  localStorage.setItem("theme", theme);
+});
