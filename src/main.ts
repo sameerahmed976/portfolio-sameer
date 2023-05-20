eac62473fa5615319dc07bca57eae7af0dac6007
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2023-05-20 11:09:44 +0530

    finished v2.0

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,54 @@ import "./css/styles.scss";
 const menuIcon = document.querySelector(".menu");
 const themeIcon = document.querySelector(".theme__icon");
 
+const navbarLinks = document.querySelectorAll(".scroll__link");
+
+// const header = document.querySelector(".header");
+
 const btnClose = document.querySelector(".btn--close");
 
 const overlay = document.querySelector(".overlay");
+
+/* navbar links */
+
+// console.log(navbarLinks);
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    overlay?.classList.remove("overlay__hide");
+
+    // @ts-ignore
+    const id = e.target.getAttribute("href")?.slice(1);
+    const element = document.getElementById(id);
+    //
+    // console.log(element);
+
+    // @ts-ignore
+    let position = element.offsetTop - 62;
+    // console.log(position);
+
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: position,
+      behavior: "smooth",
+    });
+  });
+});
+
+/* navbar links end */
+
+/* overlay  */
 
 btnClose?.addEventListener("click", () => {
   overlay?.classList.remove("overlay__hide");
 });
 
+/* overlay end */
+
 const newDate = document.querySelector(".date");
-const header = document.querySelector(".header");
+
+/* slide to top up */
 
 const slideTop = document.querySelector(".slide__top");
 
@@ -24,20 +62,21 @@ slideTop?.addEventListener("click", () => {
 });
 
 window.addEventListener("scroll", function () {
+  // if (window.pageYOffset > 10) {
+  //   header?.classList.add("header__fixed");
+  // } else {
+  //   header?.classList.remove("header__fixed");
+  // }
   if (window.pageYOffset > 80) {
     slideTop?.classList.add("slide__top__active");
-    header?.classList.add("header__fixed");
   } else {
     slideTop?.classList.remove("slide__top__active");
-    header?.classList.remove("header__fixed");
   }
 });
+/* slide to top up  end */
 
-const prefersColorScheme = window.matchMedia("(prefers-color-scheme : dark)");
+/* const prefersColorScheme = window.matchMedia("(prefers-color-scheme : dark)");
 
-menuIcon?.addEventListener("click", () => {
-  overlay?.classList.add("overlay__hide");
-});
 
 const currentTheme = localStorage.getItem("theme");
 
@@ -68,8 +107,60 @@ themeIcon?.addEventListener("click", () => {
   }
 
   localStorage.setItem("theme", theme);
+}); */
+
+menuIcon?.addEventListener("click", () => {
+  overlay?.classList.add("overlay__hide");
 });
 
-/* date  */
+/* get  current color */
+function getCurrentColor() {
+  let theme = window.matchMedia("(prefers-color-scheme : dark)").matches
+    ? "dark"
+    : "light";
+
+  localStorage.getItem("theme")
+    ? /* @ts-ignore */
+      (theme = localStorage.getItem("theme"))
+    : null;
+
+  return theme;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadTheme(getCurrentColor());
+});
+
+/* @ts-ignore */
+
+function loadTheme(theme) {
+  const root = document.querySelector(":root");
+
+  if (theme === "light") {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+
+  root?.setAttribute("color-scheme", `${theme}`);
+}
+
+themeIcon?.addEventListener("click", () => {
+  // console.log("click");
+  let theme = getCurrentColor();
+
+  // console.log(theme);
+
+  if (theme === "dark") {
+    theme = "light";
+  } else if (theme === "light") {
+    theme = "dark";
+  }
+  themeIcon.classList.toggle("theme__icon__dark");
+  localStorage.setItem("theme", theme);
+  loadTheme(theme);
+});
 
 (newDate as HTMLElement).innerHTML = `${new Date().getFullYear()} `;
